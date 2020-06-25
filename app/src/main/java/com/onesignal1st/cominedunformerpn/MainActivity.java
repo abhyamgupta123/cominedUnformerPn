5dc8d3abdd04cb0c845df7cf7a963d26e7aa1dfb
abhyamgupta123 <abhyamgupta123@gmail.com>
2020-06-25 18:23:23 +0530

    Initial commit

---
+++ app/src/main/java/com/onesignal1st/cominedunformerpn/MainActivity.java
@@ -0,0 +1,27 @@
+package com.onesignal1st.cominedunformerpn;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.onesignal.OneSignal;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // OneSignal Initialization
+        OneSignal.startInit(this)
+                .inFocusDisplaying(OneSignal.OSInFocusDisplayOption.Notification)
+                .unsubscribeWhenNotificationsAreDisabled(true)
+                .init();
+    }
+}
+
+
+
+
+
